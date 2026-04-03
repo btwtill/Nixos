306b338d5a0c7081eaedf0d26a9569254bcfee4b
Till <Till.Pfarr@web.de>
2026-04-03 21:16:11 +0200

    edit configurations

--- nixos-config/hosts/vm/configuration.nix
+++ nixos-config/hosts/vm/configuration.nix
@@ -5,28 +5,50 @@
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
+  nixpkgs.overlays = [
+    (final: prev: {
+      qtile-unwrapped = prev.qtile-unwrapped.overrideAttrs (_: {
+        doCheck = false;
+      });
+    })
+  ];
+
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Host
   networking.hostName = "vm";
+  networking.networkmanager.enable = true;
   time.timeZone = "Europe/Berlin";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  console = {
+    font = "Lat2-Terminus16";
+    useXkbConfig = true;
+  };
 
   # Enable X11 + Qtile
   services.xserver.enable = true;
+  services.xserver.layout = "de";
 
-  services.xserver.displayManager.startx.enable = true;
   services.xserver.windowManager.qtile.enable = true;
 
   # Basic packages for Qtile usability
   environment.systemPackages = with pkgs; [
-    xterm
-    dmenu
+    python313Packages.qtile
+    alacritty
+    chromium
+    picom
+    rofi
+    nitrogen
+    xfce.mousepad
     git
     neovim
-    python313Packages.qtile
   ];
+
+  programs.thunar.enable = true;
 
   # User
   users.users.defaultUser = {
