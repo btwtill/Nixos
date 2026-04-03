3fead85e2342d13d8fe87310c37a834596df876e
Till <Till.Pfarr@web.de>
2026-04-03 18:10:59 +0200

    fix defualt user name

--- nixos-config/flake.nix
+++ nixos-config/flake.nix
@@ -30,7 +30,7 @@
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
-            home-manager.users.yourusername = import ./home/user.nix;
+            home-manager.users.defaultUser = import ./home/user.nix;
           }
         ]
         # ↓ Only add the disko config module if one was provided for this host
